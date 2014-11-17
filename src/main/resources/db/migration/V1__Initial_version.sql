a4e7ed172b8d89348c21a4b9f9c4f1bb6cb37759
jkun <jkun@b4404d53-4526-0410-8e49-942741b443e7>
2014-11-17 13:22:48 +0000

    define skos scheme and collection
    git-svn-id: http://source.ktl.fi/svn/tjy/tiki/termed/trunk@13531 b4404d53-4526-0410-8e49-942741b443e7

--- src/main/resources/db/migration/V1__Initial_version.sql
+++ src/main/resources/db/migration/V1__Initial_version.sql
@@ -1,13 +1,26 @@
 
+CREATE TABLE termed.concept_scheme (
+    id varchar(255) PRIMARY KEY NOT NULL
+);
+
 CREATE TABLE termed.concept (
     id varchar(255) PRIMARY KEY NOT NULL,
-    type_id varchar(255) REFERENCES termed.concept(id),
-    parent_id varchar(255) REFERENCES termed.concept(id)
+    scheme_id varchar(255) REFERENCES termed.concept_scheme(id),
+    broader_id varchar(255) REFERENCES termed.concept(id)
 );
 
 CREATE TABLE termed.concept_related (
     concept_id varchar(255) REFERENCES termed.concept(id),
     related_id varchar(255) REFERENCES termed.concept(id)
+);
+
+CREATE TABLE termed.collection (
+    id varchar(255) PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE termed.collection_member (
+    collection_id varchar(255) REFERENCES termed.collection(id),
+    concept_id varchar(255) REFERENCES termed.concept(id)
 );
 
 CREATE TABLE termed.property (
