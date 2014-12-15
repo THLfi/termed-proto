4417499ea27462939feb2327ea46f7ca792b0d3f
jkun <jkun@b4404d53-4526-0410-8e49-942741b443e7>
2014-12-15 11:54:48 +0000

    don't use absolute url in tree source
    git-svn-id: http://source.ktl.fi/svn/tjy/tiki/termed/trunk@13773 b4404d53-4526-0410-8e49-942741b443e7

--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -14,7 +14,7 @@ App.directive('thlConceptTree', function($location) {
               data: {
                 url: function(node) {
                   var id = node.id == '#' ? c.id : node.li_attr.conceptId
-                  return '/api/concepts/' + id + '/trees';
+                  return 'api/concepts/' + id + '/trees';
                 },
                 data: function(node) {
                   return node;
