d540d49609f8dc67ec214deaa2a6cd68537cb9da
jkun <jkun@b4404d53-4526-0410-8e49-942741b443e7>
2015-05-07 06:45:24 +0000

    fix migration to run on postgres and H2
    git-svn-id: http://source.ktl.fi/svn/tjy/tiki/termed/trunk@14941 b4404d53-4526-0410-8e49-942741b443e7

--- src/main/resources/db/migration/V10__Rename_broader_table.sql
+++ src/main/resources/db/migration/V10__Rename_broader_table.sql
@@ -1,8 +1,16 @@
+CREATE TABLE ${schema}.concept_broader AS
+    (SELECT broader_id AS concept_id, narrower_id AS broader_id
+       FROM ${schema}.concept_broader_narrower);
 
-ALTER TABLE ${schema}.concept_broader_narrower ALTER COLUMN broader_id RENAME TO concept_id;
-ALTER INDEX concept_broader_narrower_broader_id_idx RENAME TO concept_broader_concept_id_idx;
+ALTER TABLE ${schema}.concept_broader ADD CONSTRAINT concept_broader_concept_id_fkey
+    FOREIGN KEY (concept_id) REFERENCES ${schema}.concept(id);
 
-ALTER TABLE ${schema}.concept_broader_narrower ALTER COLUMN narrower_id RENAME TO broader_id;
-ALTER INDEX concept_broader_narrower_narrower_id_idx RENAME TO concept_broader_broader_id_idx;
+ALTER TABLE ${schema}.concept_broader ADD CONSTRAINT concept_broader_broader_id_fkey
+    FOREIGN KEY (broader_id) REFERENCES ${schema}.concept(id);
 
-ALTER TABLE ${schema}.concept_broader_narrower RENAME TO ${schema}.concept_broader;
+CREATE INDEX concept_broader_concept_id_idx ON ${schema}.concept_broader(concept_id);
+CREATE INDEX concept_broader_broader_id_idx ON ${schema}.concept_broader(broader_id);
+
+DROP INDEX concept_broader_narrower_broader_id_idx;
+DROP INDEX concept_broader_narrower_narrower_id_idx;
+DROP TABLE ${schema}.concept_broader_narrower;
