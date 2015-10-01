27ae5668240b8b88ab067db2d56ed4a7bff7d343
jkun <jkun@b4404d53-4526-0410-8e49-942741b443e7>
2015-10-01 21:00:29 +0000

    excel export cell styles
    git-svn-id: http://source.ktl.fi/svn/tjy/tiki/termed/trunk@15586 b4404d53-4526-0410-8e49-942741b443e7

--- src/main/java/fi/thl/termed/util/ExcelTableWriter.java
+++ src/main/java/fi/thl/termed/util/ExcelTableWriter.java
@@ -1,6 +1,7 @@
 package fi.thl.termed.util;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -14,42 +15,52 @@ import java.util.List;
 
 public class ExcelTableWriter implements TableWriter {
 
-    private Logger log = LoggerFactory.getLogger(getClass());
+  private Logger log = LoggerFactory.getLogger(getClass());
 
-    private OutputStream out;
-    private Workbook wb;
-    private Sheet sheet;
+  private OutputStream out;
+  private Workbook wb;
+  private Sheet sheet;
 
-    public ExcelTableWriter(OutputStream out) {
-        this.out = out;
-        this.wb = new SXSSFWorkbook();
-        this.sheet = wb.createSheet();
+  private CellStyle cellStyle;
+
+  public ExcelTableWriter(OutputStream out) {
+    this.out = out;
+    this.wb = new SXSSFWorkbook();
+    this.sheet = wb.createSheet();
+    this.cellStyle = wb.createCellStyle();
+
+    cellStyle.setWrapText(true);
+    cellStyle.setVerticalAlignment(CellStyle.VERTICAL_TOP);
+  }
+
+  @Override
+  public void write(List<String[]> data) {
+    int rowNumber = 0;
+    for (String[] rowData : data) {
+      writeRow(sheet.createRow(rowNumber), rowData);
+      sheet.autoSizeColumn(rowNumber);
+      rowNumber++;
     }
+    writeAndClose();
+  }
 
-    @Override
-    public void write(List<String[]> data) {
-        int rows = 0;
-        for (String[] rowData : data) {
-            writeRow(sheet.createRow(rows++), rowData);
-        }
-        writeAndClose();
+  private void writeRow(Row row, String[] rowData) {
+    for (int i = 0; i < rowData.length; i++) {
+      Cell cell = row.createCell(i);
+      cell.setCellType(Cell.CELL_TYPE_STRING);
+      cell.setCellValue(rowData[i]);
+      cell.setCellStyle(cellStyle);
     }
+    row.setHeight((short) 700);
+  }
 
-    private void writeRow(Row row, String[] rowData) {
-        for (int i = 0; i < rowData.length; i++) {
-            Cell cell = row.createCell(i);
-            cell.setCellType(Cell.CELL_TYPE_STRING);
-            cell.setCellValue(rowData[i]);
-        }
+  private void writeAndClose() {
+    try {
+      wb.write(out);
+      out.close();
+    } catch (IOException e) {
+      log.error("", e);
     }
-
-    private void writeAndClose() {
-        try {
-            wb.write(out);
-            out.close();
-        } catch (IOException e) {
-            log.error("", e);
-        }
-    }
+  }
 
 }
