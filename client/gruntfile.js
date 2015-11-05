ea1721836afbdad365ac8d19b108f06ba55609c8
Jussi Kurki <jussi.kurki@iki.fi>
2015-11-05 18:08:58 +0200

    testing dev env for front end

--- client/gruntfile.js
+++ client/gruntfile.js
@@ -6,15 +6,43 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-ng-constant');
   grunt.loadNpmTasks('grunt-wiredep');
   grunt.loadNpmTasks('grunt-karma');
 
-  grunt.registerTask('default', ['jshint', 'karma', 'wiredep', 'copy']);
+  grunt.registerTask('default', ['ngconstant:prod', 'jshint', 'karma:unit', 'wiredep', 'copy']);
+  grunt.registerTask('dev', ['ngconstant:dev', 'connect:server', 'karma:watch']);
 
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
     bower: grunt.file.readJSON('.bowerrc'),
+
+    ngconstant: {
+      options: {
+        wrap: "(function(angular) { 'use strict';\n\n{%= __ngModule %}\n\n})(window.angular);",
+        name: 'termed.config',
+        dest: 'src/app/config.js'
+      },
+      dev: {
+        constants: {
+          apiUrl: 'http://localhost:9999'
+        }
+      },
+      prod: {
+        constants: {
+          apiUrl: ''
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        port: 9999,
+        base: 'src'
+      }
+    },
 
     wiredep: {
       task: {
@@ -43,6 +71,19 @@ module.exports = function(grunt) {
           frameworks: ['jasmine'],
           singleRun: true,
           browsers: ['PhantomJS'],
+          files: [
+            'src/lib/angular/angular.js',
+            'src/lib/angular-mocks/angular-mocks.js',
+            'test/**/*.js'
+          ]
+        }
+      },
+      watch: {
+        options: {
+          frameworks: ['jasmine'],
+          singleRun: false,
+          autoWatch: true,
+          browsers: ['Chrome'],
           files: [
             'src/lib/angular/angular.js',
             'src/lib/angular-mocks/angular-mocks.js',
---
+++ client/src/app/config.js
@@ -0,0 +1,9 @@
+(function(angular) { 'use strict';
+
+angular.module('termed.config', [])
+
+.constant('apiUrl', 'http://localhost:9999')
+
+;
+
+})(window.angular);
